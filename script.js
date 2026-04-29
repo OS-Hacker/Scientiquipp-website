910be8ef676cf0df872ac8fea5c5bf730c039078
Om Shinde <omshinde3724@gmail.com>
2026-04-29 18:26:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// Script to handle active state of nav links
+const links = document.querySelectorAll(".nav-link");
+
+links.forEach(link => {
+  link.addEventListener("click", function () {
+    links.forEach(l => l.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
